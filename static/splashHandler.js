45c3e0f290b70641e6e291a547aba9fd045bedd6
unit4216 <pf.paliza@gmail.com>
2022-02-13 16:49:46 -0800

    fixed issue with splash screen showing briefly on refresh

--- static/splashHandler.js
+++ static/splashHandler.js
@@ -1,6 +1,10 @@
+//handles splash screen based on whether it's a new session
 $(function() {
 
     if(localStorage.getItem('firstVisit')==null){
+        //show splash screen
+        document.getElementById('splash').style.display = 'flex';
+
         //splash screen "enter" function, fades out splash screen and fades in app and allows keypress event listener to function
         $('#start').click(function() { 
             localStorage.setItem('ready', true);    //enable keypress input
